000774364f3a79a188c0076295e12e053a6c523c
gpri989 <gpriyanshu903@gmail.com>
2021-06-24 04:19:55 +0000

    Bringup Octavi!

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_gauguin.mk
+    $(LOCAL_DIR)/octavi_gauguin.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_gauguin-user \
-    arrow_gauguin-userdebug \
-    arrow_gauguin-eng
+    octavi_gauguin-user \
+    octavi_gauguin-userdebug \
+    octavi_gauguin-eng
--- octavi_gauguin.mk
+++ octavi_gauguin.mk
@@ -7,17 +7,17 @@
 # Inherit from gauguin device
 $(call inherit-product, device/xiaomi/gauguin/device.mk)
 
-# Inherit some common Arrow stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
-TARGET_USES_AOSP_RECOVERY := true
+# Inherit some common Octavi stuff.
+$(call inherit-product, vendor/octavi/config/common_full_phone.mk)
+
+#Bootanimation
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm64
-TARGET_INCLUDE_STOCK_ARCORE := true
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
-TARGET_FACE_UNLOCK_SUPPORTED := true
+
+OCTAVI_BUILD_TYPE := Official
+OCTAVI_DEVICE_MAINTAINER := Priyanshu
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_gauguin
+PRODUCT_NAME := octavi_gauguin
 PRODUCT_DEVICE := gauguin
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
